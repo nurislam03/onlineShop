52db45f99754a1a355731918953e1d1a88dc8094
Nur Islam <nurislam0333@gmail.com>
2020-11-04 23:39:18 +0600

    fix late render issue

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e76838f092e28f6d9be5";
+/******/ 	var hotCurrentHash = "8c070c0d7d50d15923ea";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
